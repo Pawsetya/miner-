4682914d7b68fe8e556d5c157cea79eed1cdd2f6
Pawsetya <98332550+Pawsetya@users.noreply.github.com>
2022-01-24 22:11:21 +0700

    Create Verus.sh

---
+++ Verus.sh
@@ -0,0 +1,6 @@
+1 #verus coin
+2 sudo apt update
+3 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+4 tar xf hellminer_cpu_linux.tar.gz
+5 ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RJTz7xKSwnhRGcgih3rg5GeKfhxMqbjLyn.paw -p x --cpu 4
+
